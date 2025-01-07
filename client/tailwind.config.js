37148250484deeefeb89e676dd7d555c7f72c3e6
mpawank <mpawankumar356@gmai.com>
2025-01-07 16:06:04 +0530

    first commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Add your HTML file
+    "./src/**/*.{js,jsx,ts,tsx}", // Add all JS, JSX, TS, and TSX files in your src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
